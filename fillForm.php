2f5d2f71159bc398ed3d2097654cc1cf315dd2e3
Ian MacLeod <imacleod@mail.umw.edu>
2024-10-12 14:48:25 -0400

    Added logo and changed some text

--- fillForm.php
+++ fillForm.php
@@ -12,16 +12,16 @@ error_reporting(E_ALL);
 <html>
     <head>
         <?php require('universal.inc'); ?>
-        <title>Stafford Junction Homepage</title>
+        <title>Stafford Junction | Forms</title>
     </head>
     <body>
         <?php require('header.php'); ?>
-        <h1>Stafford Junction Dashboard</h1>
+        <h1>Forms</h1>
         <main class='dashboard'>
             <div id="dashboard">
                 <div class="dashboard-item" data-link="holidayMealBagForm.php">
-                    <img src="images/logout.svg">
-                    <span>Log out</span>
+                    <img src="images/holdiayMealBagIcon.svg">
+                    <span>Holiday Meal Bag Form</span>
                 </div>
             </div>
         </main>
