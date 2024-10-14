b9ef75021d4a2e44f2cd4c03179e52f54f54f7cc
Ian MacLeod <imacleod@mail.umw.edu>
2024-10-14 13:29:58 -0400

    Added school supplies html template to dashboard

---
+++ schoolSuppliesForm.php
@@ -0,0 +1,16 @@
+<?php
+
+
+?>
+
+<html>
+    <head>
+        <?php include_once("universal.php")?>
+        <title>School Supplies Form</title>
+    </head>
+    <body>
+
+        <h1>School Supplies Form</h1>
+        
+    </body>
+</html>
