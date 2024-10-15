af70cccffa4fc587dc618bea3dfeec398470bd52
Brandon <brogers5260@gmail.com>
2024-10-15 01:37:53 -0400

    Added forgotPassword.php file which reuses much of the code from changePassword.php

---
+++ forgotPassword.php
@@ -0,0 +1,51 @@
+<?php
+    session_cache_expire(30);
+    session_start();
+    require_once('include/api.php');
+    ini_set("display_errors",1);
+    error_reporting(E_ALL);
+    $accessLevel = 0;
+    $userID = null;
+    if (isset($_SESSION['_id'])) {
+        // 0 = not logged in, 1 = standard user, 2 = manager (Admin), 3 super admin (TBI)
+        $accessLevel = $_SESSION['access_level'];
+        $userID = $_SESSION['_id'];
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        require_once('include/input-validation.php');
+        require_once('domain/Person.php');
+        require_once('database/dbPersons.php');
+        if (!wereRequiredFieldsSubmitted($_POST, array('password', 'new-password'))) {
+            echo "Args missing";
+            die();
+        }
+        $newPassword = $_POST['new-password'];
+        $user = retrieve_person($userID);
+        $hash = password_hash($newPassword, PASSWORD_BCRYPT);
+        change_password($userID, $hash);
+        header('Location: index.php?pcSuccess');
+        die();
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require_once('universal.inc') ?>
+        <title>Stafford Junction | Change Password</title>
+    </head>
+    <body>
+        <?php require_once('header.php') ?>
+        <h1>Change Password</h1>
+        <main class="login">
+            <form id="password-change" method="post">
+                <label for="new-password">New Password</label>
+                <input type="password" id="new-password" name="new-password" placeholder="Enter new password" required>
+                <label for="reenter-new-password">New Password</label>
+                <input type="password" id="new-password-reenter" placeholder="Re-enter new password" required>
+                <p id="password-match-error" class="error hidden">Passwords must match!</p>
+                <input type="submit" id="submit" name="submit" value="Change Password">
+            </form>
+        </main>
+    </body>
+</html>
