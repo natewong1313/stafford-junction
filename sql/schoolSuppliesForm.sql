286ad128c0ca4d9dbcd83dcb1ff0134e634030ad
Brandon <brogers5260@gmail.com>
2024-10-16 10:34:27 -0400

    Fixed some issues with the table definition

--- sql/schoolSuppliesForm.sql
+++ sql/schoolSuppliesForm.sql
@@ -3,7 +3,7 @@
 --
 
 CREATE TABLE `dbSchoolSuppliesForm` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `child_id` int(11) NOT NULL,
   `email` varchar(256) NOT NULL,
   `child_name` varchar(256) NOT NULL,
@@ -12,18 +12,6 @@ CREATE TABLE `dbSchoolSuppliesForm` (
   `bag_pickup_method` text NOT NULL,
   `need_backpack` boolean NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- AUTO_INCREMENT for table `dbSchoolSuppliesForm`
---
-ALTER TABLE `dbSchoolSuppliesForm`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT;
-
---
--- Indexes for table `dbSchoolSuppliesForm`
---
-ALTER TABLE `dbSchoolSuppliesForm`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Constraints for table `dbSchoolSuppliesForm`, change dbAnimals to dbChildren
