edb30192b8dd5e7c7846125dd9e4179a4bcd7f1b
Nate Wong <natewong1@gmail.com>
2024-10-24 12:19:01 -0400

    Add more columns and fix casing of data types

--- sql/3updateDbPersons.sql
+++ sql/3updateDbPersons.sql
@@ -7,20 +7,32 @@ ALTER TABLE `dbPersons`
     DROP COLUMN `id`;
 /* now we modify id to be type of int and also need to update dbEventVolunteers */
 ALTER TABLE `dbPersons`
-    ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+    ADD COLUMN `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY;
 ALTER TABLE `dbEventVolunteers`
-    CHANGE COLUMN `userID` `userID` INT NOT NULL;
+    CHANGE COLUMN `userID` `userID` int NOT NULL;
 ALTER TABLE `dbEventVolunteers`
   ADD CONSTRAINT `FKpersonID` FOREIGN KEY (`userID`) REFERENCES `dbPersons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /* now add new columns */
 ALTER TABLE `dbPersons`
-    ADD COLUMN `security_question` TEXT NOT NULL,
-    ADD COLUMN `security_answer` TEXT NOT NULL,
-    ADD COLUMN `emergency_first_name` TEXT NOT NULL,
-    ADD COLUMN `emergency_last_name` TEXT NOT NULL,
-    ADD COLUMN `emergency_phone` TEXT NOT NULL,
-    ADD COLUMN `emergency_relation` TEXT NOT NULL,
-    ADD COLUMN `is_active` BOOLEAN NOT NULL DEFAULT 1;
+    ADD COLUMN `secondary_first_name` text,
+    ADD COLUMN `secondary_last_name` text,
+    ADD COLUMN `secondary_birthday` varchar(10),
+    ADD COLUMN `secondary_address` text,
+    ADD COLUMN `secondary_city` text,
+    ADD COLUMN `secondary_state` text,
+    ADD COLUMN `secondary_zip` int,
+    ADD COLUMN `secondary_email` varchar(256),
+    ADD COLUMN `secondary_phone1` varchar(12),
+    ADD COLUMN `secondary_phone1_type` varchar(256),
+    ADD COLUMN `secondary_phone2` varchar(12),
+    ADD COLUMN `secondary_phone2_type` varchar(256),
+    ADD COLUMN `security_question` text NOT NULL,
+    ADD COLUMN `security_answer` text NOT NULL,
+    ADD COLUMN `emergency_first_name` text NOT NULL,
+    ADD COLUMN `emergency_last_name` text NOT NULL,
+    ADD COLUMN `emergency_phone` text NOT NULL,
+    ADD COLUMN `emergency_relation` text NOT NULL,
+    ADD COLUMN `is_active` boolean NOT NULL DEFAULT 1;
 
 COMMIT;
