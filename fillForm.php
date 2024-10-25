1e2281d150e836ad9085b268af2b18409c740f55
Josh Williams <joshwilliams@Joshs-MacBook-Pro.local>
2024-10-25 09:01:25 -0400

    sprint 2 added field trip waiver

--- fillForm.php
+++ fillForm.php
@@ -37,6 +37,10 @@ error_reporting(E_ALL);
                     <img src="images/angel.svg">
                     <span>Angel Gifts Wish Form</span> </div>
 
+                    <div class="dashboard-item" data-link="fieldTripWaiver.php">
+                    <img src="images/location.svg">
+                    <span>Field Trip Waiver Form</span> </div>
+
 
                 </div>
             </div>
