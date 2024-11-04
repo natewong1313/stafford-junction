b2dd2eb15149b63bca1497f0eabd1fdc9e3dda4b
Anna Swann <128412269+aswann2@users.noreply.github.com>
2024-11-04 18:35:21 -0500

    [SJ-64] Create the html for the 'New Summer Junction Registration FNL Packet' Form (#61)
    * Jumpstart ticket
    
    * Add spanish sections
    
    * Add icon and form to forms dashboard
    
    ---------
    
    Co-authored-by: Anna Swann <aswann2@mail.umw.edu>

--- fillForm.php
+++ fillForm.php
@@ -26,44 +26,59 @@ error_reporting(E_ALL);
 
                 <div class="dashboard-item" data-link="schoolSuppliesForm.php">
                     <img src="images/school-supplies-svgrepo-com.svg">
-                    <span>School Supplies Form</span> </div>
-
+                    <span>School Supplies Form</span> 
+                </div>
 
                 <div class="dashboard-item" data-link="springBreakForm.php">
                     <img src="images/tent-svgrepo-com.svg">
-                    <span>Spring Break Form</span> </div>
+                    <span>Spring Break Form</span> 
+                
+                </div>
 
                 <div class="dashboard-item" data-link="angelGiftForm.php">
                     <img src="images/angel.svg">
-                    <span>Angel Gifts Wish Form</span> </div>
+                    <span>Angel Gifts Wish Form</span> 
+                </div>
 
                 <div class="dashboard-item" data-link="actualActivityForm.php">
                     <img src="images/actualActivity-svgrepo.svg">
-                    <span>Actual Activity Form</span> </div>
+                    <span>Actual Activity Form</span> 
+                </div>
 
                 <div class="dashboard-item" data-link="childCareWaiverForm.php">
                     <img src="images/signature.svg">
-                    <span>Child Care Waiver Form</span> </div>
+                    <span>Child Care Waiver Form</span> 
+                </div>
 
                 <div class="dashboard-item" data-link="fieldTripWaiver.php">
                     <img src="images/location.svg">
-                    <span>Field Trip Waiver Form</span> </div>
+                    <span>Field Trip Waiver Form</span> 
+                </div>
 
                 <div class="dashboard-item" data-link="programInterestForm.php">
                     <img src="images/interest.svg">
-                    <span>Program Interest Form</span> </div>
+                    <span>Program Interest Form</span> 
+                </div>
 
                 <div class="dashboard-item" data-link="brainBuildersRegistrationForm.php">
                     <img src="images/brainBuilders.svg">
-                    <span>Brain Builders Student Registration Form</span> </div>
+                    <span>Brain Builders Student Registration Form</span> 
+                </div>
+
                 <div class="dashboard-item" data-link="BrainBuilderReviewForm.php">
                     <img src="images/brainBuilderReviewIcon.svg.svg">
-                    <span>Brain Builder Review Form</span> </div>
+                    <span>Brain Builder Review Form</span> 
+                </div>
 
                 <div class="dashboard-item" data-link="holidayPartyForm.php">
                     <img src="images/party-flyer-svgrepo-com.svg">
-                    <span>Brain Builders Holiday Party Form</span> </div>
+                    <span>Brain Builders Holiday Party Form</span> 
+                </div>
 
+                <div class="dashboard-item" data-link="summerJunctionRegistrationForm.php">
+                    <img src="images/summerJunction.svg">
+                    <span>Summer Junction Registration Form</span>
+                </div>
 
                 </div>
             </div>
