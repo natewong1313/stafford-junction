f07e83bdaf5ee9be551dfdc00fe5b48f5a6c4344
Brandon <brogers5260@gmail.com>
2024-11-10 16:26:22 -0500

    Added few lines of code and comments. Also put the SQL queries in dbProgramInterestForm.php inside a mysqli transaction

--- database/dbProgramInterestForm.php
+++ database/dbProgramInterestForm.php
@@ -17,41 +17,50 @@ function createProgramInterestForm($form) {
     $child_num = $form["child_num"];
     $child_ages = $form["child_ages"];
     $adult_num = $form["adult_num"];
-	
-    // First query inserts basic information in dbProgramInterestForm
-    $query = "
-        INSERT INTO dbProgramInterestForm (family_id, first_name, last_name, address, city, neighborhood, state, zip, cell_phone, home_phone, email, child_num, 
-        child_ages, adult_num)
-        values ('$family_id', '$first_name', '$last_name', '$address', '$city', '$neighborhood', '$state', '$zip', '$cell_phone', '$home_phone', '$email', '$child_num', 
-        '$child_ages', '$adult_num');
-    ";
-    $result = mysqli_query($connection, $query);
-    if (!$result) {
+	$id = null;
+
+    // Begin transaction so if there are any errors with any queries, they are all rolled back and no data is inserted into database
+    // This is done so if one query is succesful and another isn't, it doesn't just insert the successful one which would result in incomplete data in the database
+    mysqli_begin_transaction($connection);
+    try {
+        // First query inserts basic information in dbProgramInterestForm
+        $query = "
+            INSERT INTO dbProgramInterestForm (family_id, first_name, last_name, address, city, neighborhood, state, zip, cell_phone, home_phone, email, child_num, 
+            child_ages, adult_num)
+            values ('$family_id', '$first_name', '$last_name', '$address', '$city', '$neighborhood', '$state', '$zip', '$cell_phone', '$home_phone', '$email', '$child_num', 
+            '$child_ages', '$adult_num');
+        ";
+        $result = mysqli_query($connection, $query);
+        if (!$result) {
+            return null;
+        }
+        $id = mysqli_insert_id($connection);
+
+        // Insert program interests
+        if (isset($form['programs'])) {
+            $programs = $form['programs'];
+            insertProgramInterests($programs, $id, $connection);
+        }
+        // Insert topic interests
+        if (isset($form['topics'])) {
+            $topics = $form['topics'];
+            insertTopicInterests($topics, $id, $connection);
+        }
+        // insert availability
+        $days = $form['days'];
+        insertAvailability($days, $id, $connection);
+        // Commit transaction if there were no problems inserting data
+        mysqli_commit($connection);
+    } catch (Exception $e) {
+        // If there was a problem inserting data roll back and return null
+        mysqli_rollback($connection);
         return null;
     }
-    $id = mysqli_insert_id($connection);
-    mysqli_commit($connection);
     mysqli_close($connection);
-
-    // Insert program interests
-    if (isset($args['programs'])) {
-        $programs = $args['programs'];
-        insertProgramInterests($programs, $id);
-    }
-    // Insert topic interests
-    if (isset($args['topics'])) {
-        $topics = $args['topics'];
-        insertProgramInterests($topics, $id);
-    }
-    // insert availability
-    $days = $form['days'];
-    insertAvailability($days, $id);
     return $id;
 }
 
-function insertProgramInterests($programs, $form_id) {
-    $connection = connect();
-	
+function insertProgramInterests($programs, $form_id, $connection) {
     // Second query inserts ids in dbProgramInterestsForm_ProgramInterests junction table
     $query = "INSERT INTO dbProgramInterestsForm_ProgramInterests (form_id, interest_id) values ";
     $last = end($programs);
@@ -69,14 +78,10 @@ function insertProgramInterests($programs, $form_id) {
         return null;
     }
     $id = mysqli_insert_id($connection);
-    mysqli_commit($connection);
-    mysqli_close($connection);
     return $id;
 }
 
-function insertTopicInterests($topics, $form_id) {
-    $connection = connect();
-	
+function insertTopicInterests($topics, $form_id, $connection) {
     // Third query inserts ids in dbProgramInterestsForm_TopicInterests junction table
     $query = "INSERT INTO dbProgramInterestsForm_TopicInterests (form_id, interest_id) values ";
     $last = end($topics);
@@ -99,14 +104,10 @@ function insertTopicInterests($topics, $form_id) {
         return null;
     }
     $id = mysqli_insert_id($connection);
-    mysqli_commit($connection);
-    mysqli_close($connection);
     return $id;
 }
 
-function insertAvailability($days, $form_id) {
-    $connection = connect();
-	
+function insertAvailability($days, $form_id, $connection) {
     // Third query inserts family availability data in dbAvalability
     $query = "INSERT INTO dbAvailability (form_id, day, morning, afternoon, evening, specific_time) values ";
     $last = array_key_last($days);
@@ -135,9 +136,6 @@ function insertAvailability($days, $form_id) {
         return null;
     }
     $id = mysqli_insert_id($connection);
-    mysqli_commit($connection);
-    mysqli_close($connection);
     return $id;
-
 }
 ?>
