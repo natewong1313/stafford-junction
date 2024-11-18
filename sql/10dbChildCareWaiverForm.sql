74b8b43cea01c0ce95208a2a1aceecfd2e8f7a0d
Josh Williams <joshwilliams@Joshs-MBP.fios-router.home>
2024-11-18 12:25:33 -0500

    added drop table if exist for child care waiver form

--- sql/10dbChildCareWaiverForm.sql
+++ sql/10dbChildCareWaiverForm.sql
@@ -1,43 +1,38 @@
-CREATE TABLE dbChildCareWaiverForm (
+DROP TABLE IF EXISTS dbFieldTripWaiverForm;
+
+CREATE TABLE dbFieldTripWaiverForm (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     child_id INT NOT NULL,
     child_first_name VARCHAR(256),
     child_last_name VARCHAR(256),
-    birth_date DATE,
     gender VARCHAR(6),
+    birth_date DATE,
+    neighborhood VARCHAR(256),
+    school VARCHAR(256),
     child_address VARCHAR(256),
     child_city VARCHAR(100),
     child_state VARCHAR(100),
-    child_zip VARCHAR(10),
+    child_zip VARCHAR(10), 
+    parent_email VARCHAR(256),
 
-    parent_1 INT NOT NULL,
-    parent1_first_name VARCHAR(256),
-    parent1_last_name VARCHAR(256),
-    parent1_address VARCHAR(256),
-    parent1_city VARCHAR(100),
-    parent1_state VARCHAR(100),
-    parent1_zip_code VARCHAR(10),
-    parent1_email VARCHAR(256),
-    parent1_cell_phone VARCHAR(15),
-    parent1_home_phone VARCHAR(15),
-    parent1_work_phone VARCHAR(15),
+    contact_1 INT NOT NULL,
+    emgcy_contact1_first_name VARCHAR(256),
+    emgcy_contact1_last_name VARCHAR(256),
+    emgcy_contact1_rship VARCHAR(100),
+    emgcy_contact1_phone VARCHAR(15),
 
-    parent_2 INT, -- Updated to allow NULL, as a second parent may not always exist
-    parent2_first_name VARCHAR(256),
-    parent2_last_name VARCHAR(256),
-    parent2_address VARCHAR(256),
-    parent2_city VARCHAR(100),
-    parent2_state VARCHAR(100),
-    parent2_zip_code VARCHAR(10),
-    parent2_email VARCHAR(256),
-    parent2_cell_phone VARCHAR(15),
-    parent2_home_phone VARCHAR(15),
-    parent2_work_phone VARCHAR(15),
+    contact_2 INT, -- Made nullable in case a second emergency contact is not provided
+    emgcy_contact2_first_name VARCHAR(256),
+    emgcy_contact2_last_name VARCHAR(256),
+    emgcy_contact2_rship VARCHAR(100),
+    emgcy_contact2_phone VARCHAR(15),
 
-    parent_guardian_signature VARCHAR(256),
-    signature_date DATE,
+    medical_insurance_company VARCHAR(256),
+    policy_number VARCHAR(50),
+    photo_waiver_signature VARCHAR(256),
+    photo_waiver_date DATE,
 
-    CONSTRAINT FK_dbcwaiverform_child_id
+    CONSTRAINT FK_fieldtripwaiver_child_id
         FOREIGN KEY (child_id) REFERENCES dbChildren(id)
         ON DELETE CASCADE 
         ON UPDATE CASCADE
