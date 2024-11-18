672c8137ece5f9f4f2c1c7f73b1ce7f50a5e1170
jwilli39 <117937537+jwilli39@users.noreply.github.com>
2024-11-18 17:23:59 -0500

    update sql syntax (#93)
    * update sql syntax
    
    * added drop table if exist for the spring break form
    
    * Changed data types of isAttending and waiver_completed columns from TINYINT(1) back to BOOLEAN in dbSpringBreakCampForm
    
    ---------
    
    Co-authored-by: Josh Williams <joshwilliams@Joshs-MBP.fios-router.home>

--- sql/12dbSpringBreakCampForm.sql
+++ sql/12dbSpringBreakCampForm.sql
@@ -1,16 +1,15 @@
--- Table for the dbSpringBreakCampForm
+DROP TABLE IF EXISTS dbSpringBreakCampForm;
+
 CREATE TABLE dbSpringBreakCampForm (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email TEXT NOT NULL,
     student_name VARCHAR(256) NOT NULL,
     school_choice TEXT NOT NULL,
-    isAttending BOOLEAN NOT NULL,
-    waiver_completed BOOLEAN NOT NULL,
+    isAttending BOOLEAN NOT NULL, 
+    waiver_completed BOOLEAN NOT NULL, 
     notes TEXT,
-    
     child_id INT,
-   
-    CONSTRAINT FK_child_id
+    CONSTRAINT FK_dbspringbreakcampform_child_id
         FOREIGN KEY (child_id) REFERENCES dbChildren(id)
         ON DELETE CASCADE 
         ON UPDATE CASCADE
