fc7344736b5d3c04bda4a73f170645d06aab2075
Josh Williams <joshwilliams@Joshs-MBP.fios-router.home>
2024-11-18 12:31:10 -0500

    added drop table if exist for the spring break form

--- sql/12dbSpringBreakCampForm.sql
+++ sql/12dbSpringBreakCampForm.sql
@@ -1,11 +1,12 @@
--- Table for the dbSpringBreakCampForm
+DROP TABLE IF EXISTS dbSpringBreakCampForm;
+
 CREATE TABLE dbSpringBreakCampForm (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email TEXT NOT NULL,
     student_name VARCHAR(256) NOT NULL,
     school_choice TEXT NOT NULL,
-    isAttending BOOLEAN NOT NULL,
-    waiver_completed BOOLEAN NOT NULL,
+    isAttending TINYINT(1) NOT NULL, -- BOOLEAN is an alias for TINYINT(1)
+    waiver_completed TINYINT(1) NOT NULL, -- BOOLEAN is an alias for TINYINT(1)
     notes TEXT,
     child_id INT,
     CONSTRAINT FK_dbspringbreakcampform_child_id
