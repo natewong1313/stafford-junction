1e604f4e38d9ff0749abeff57b4975644eedcc55
selbyheyman <104396572+selbyheyman@users.noreply.github.com>
2024-11-23 13:17:57 -0500

    alter table statements

--- sql/17updateBrainBuildersRegistrationForm.sql
+++ sql/17updateBrainBuildersRegistrationForm.sql
@@ -3,6 +3,11 @@
 --
 ALTER TABLE `dbBrainBuildersRegistrationForm`
   ADD COLUMN `child_id` INT NOT NULL;
+  CHANGE COLUMN `gender` `child_gender` VARCHAR(6) NOT NULL,
+  CHANGE COLUMN `school_name` `child_school_name` VARCHAR(100) NOT NULL,
+  CHANGE COLUMN `grade` `child_grade` VARCHAR(20) NOT NULL,
+  CHANGE COLUMN `birthdate` `child_dob` DATE NOT NULL,
+  CHANGE COLUMN `needs_transportation` `transportation` ENUM('needs_transportation', 'transports_themselves') NOT NULL;
 
 --
 -- Constraints for table `dbBrainBuildersRegistrationForm`
