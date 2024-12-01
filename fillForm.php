a482ad4aa78cb73e8e05a947af78fe69eba5eebc
Nate Wong <natewong1@gmail.com>
2024-12-01 17:36:25 -0500

    Add id param to all forms

--- fillForm.php
+++ fillForm.php
@@ -26,29 +26,20 @@ error_reporting(E_ALL);
                 <!--Holiday Meal Bag -->
                 <!--will display this dashboard item if viewing the page from staff pov. This one has the GET superglobal, w
                 hich will be needed so that the id of the family is saved and able to be used when a staff memeber fills a form out on behalf of family-->
-                <?php if(isset($_GET['id'])): ?>
-                    <div class="dashboard-item" data-link="holidayMealBagForm.php?id=<?php echo $_GET['id']?>">
+                <div class="dashboard-item" data-link="<?php echo isset($_GET['id']) ? "holidayMealBagForm.php?id=" . $_GET['id'] : "holidayMealBagForm.php"; ?>">
                     <img src="images/holdiayMealBagIcon.svg">
                     <span>Holiday Meal Bag Form</span>
                 </div>
-                <?php else: ?> <!--Otherwise, display this one-->
-                <div class="dashboard-item" data-link="holidayMealBagForm.php">
-                    <img src="images/holdiayMealBagIcon.svg">
-                    <span>Holiday Meal Bag Form</span>
-                </div>
-                <?php endif?>
 
                 <!--School Supplies Form -->
-
-                <div class="dashboard-item" data-link="schoolSuppliesForm.php">
+                <div class="dashboard-item" data-link="<?php echo isset($_GET['id']) ? "schoolSuppliesForm.php?id=" . $_GET['id'] : "schoolSuppliesForm.php"; ?>">
                     <img src="images/school-supplies-svgrepo-com.svg">
-                    <span>School Supplies Form</span> 
+                    <span>School Supplies Form</span>
                 </div>
-
-                <div class="dashboard-item" data-link="springBreakForm.php">
-                    <img src="images/tent-svgrepo-com.svg">
-                    <span>Spring Break Form</span> 
                 
+                <div class="dashboard-item" data-link="<?php echo isset($_GET['id']) ? "springBreakForm.php?id=" . $_GET['id'] : "springBreakForm.php"; ?>">
+                    <img src="images/tent-svgrepo-com.svg">
+                    <span>Spring Break Form</span>
                 </div>
                     
                 <!-- Angel Wish Gift Form -->
@@ -63,7 +54,7 @@ error_reporting(E_ALL);
                     <span>Child Care Waiver Form</span> 
                 </div>
 
-                <div class="dashboard-item" data-link="fieldTripWaiver.php">
+                <div class="dashboard-item" data-link="<?php echo isset($_GET['id']) ? "fieldTripWaiver.php?id=" . $_GET['id'] : "fieldTripWaiver.php"; ?>">
                     <img src="images/location.svg">
                     <span>Field Trip Waiver Form</span> 
                 </div>
@@ -74,12 +65,12 @@ error_reporting(E_ALL);
                     <span>Program Interest Form</span> 
                 </div>
 
-                <div class="dashboard-item" data-link="brainBuildersRegistrationForm.php">
+                <div class="dashboard-item" data-link="<?php echo isset($_GET['id']) ? "brainBuildersRegistrationForm.php?id=" . $_GET['id'] : "brainBuildersRegistrationForm.php"; ?>">
                     <img src="images/brainBuilders.svg">
                     <span>Brain Builders Student Registration Form</span> 
                 </div>
 
-                <div class="dashboard-item" data-link="BrainBuilderReviewForm.php">
+                <div class="dashboard-item" data-link="<?php echo isset($_GET['id']) ? "BrainBuilderReviewForm.php?id=" . $_GET['id'] : "BrainBuilderReviewForm.php"; ?>">
                     <img src="images/brainBuilderReviewIcon.svg.svg">
                     <span>Brain Builder Review Form</span> 
                 </div>
@@ -90,7 +81,7 @@ error_reporting(E_ALL);
                     <span>Brain Builders Holiday Party Form</span> 
                 </div>
 
-                <div class="dashboard-item" data-link="summerJunctionRegistrationForm.php">
+                <div class="dashboard-item" data-link="<?php echo isset($_GET['id']) ? "summerJunctionRegistrationForm.php?id=" . $_GET['id'] : "summerJunctionRegistrationForm.php"; ?>">
                     <img src="images/summerJunction.svg">
                     <span>Summer Junction Registration Form</span>
                 </div>
